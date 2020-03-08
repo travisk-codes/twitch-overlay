1e052372fc805f0bfdeab495f975ad292660d988
Travis Kohlbeck <me+git@travisk.info>
2020-03-07 21:55:09 -0500

    working drop-shadows and ticker text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Scroll from 'react-textscroll'
+
+import './App.css'
+
+const TickerItems = () => (
+	<>
+		<div className='ticker__item text'>
+			🕒 <span style={{ color: 'lightskyblue' }}>Doing now:&nbsp;</span> getting
+			top text to scroll
+		</div>
+		<div className='ticker__item text'>
+			🕒 <span style={{ color: 'dodgerblue' }}>Doing later:&nbsp;</span> fixing
+			streamlabs alerts
+		</div>
+		<div className='ticker__item text'>
+			🕒 <span style={{ color: 'rgb(64, 64, 255)' }}>10PM EST:&nbsp;</span>{' '}
+			playing CS:GO / Trackmania
+		</div>
+		<div className='ticker__item text'>
+			📢 &nbsp;<span style={{ color: 'red' }}>Announcement:&nbsp;</span> Found
+			my missing lip balm (favorite)
+		</div>
+		<div className='ticker__item text' style={{ color: 'rgb(150, 255, 150)' }}>
+			🛣️ &nbsp;Road to Affiliate:
+		</div>
+		<div className='ticker__item text' style={{ color: 'rgb(150, 150, 255)' }}>
+			🙋🏼‍♀️ 27/50 followers (over half way there!)
+		</div>
+		<div className='ticker__item text' style={{ color: 'rgb(255, 150, 150)' }}>
+			👀 &nbsp;0.8/3 average viewers
+		</div>
+	</>
+)
+
+function TickerSets() {}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='ticker-wrap'>
+				<div className='ticker'>
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+					<TickerItems />
+				</div>
+			</div>
+			<div className='drop-shadow' id='screen-box' />
+			<div className='drop-shadow' id='camera-box' />
+		</div>
+	)
 }
 
-export default App;
+export default App
